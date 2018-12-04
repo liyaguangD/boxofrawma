c8843e41c7a916d30ab9d552df80cf484a830c4c
liyaguangD <103637208@qq.com>
2018-12-04 19:07:19 +0800

    sssssssssssssssssssssssssssssss

--- goods-server/src/main/java/com/jk/service/GoodsServiceImpl.java
+++ goods-server/src/main/java/com/jk/service/GoodsServiceImpl.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Service
 public class GoodsServiceImpl implements GoodsService {
-
+    //asdasdasdasdasdasdasdasdasdasdasdas
     @Autowired
     private GoodsMapper goodsMapper;
     @Override
